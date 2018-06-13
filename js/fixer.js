52de02beb92055796502917611ebbc124600d448
LastChanceBilly <josetecnom12@hotmail.com>
2018-06-13 17:09:07 -0400

    Initial commit

---
+++ js/fixer.js
@@ -0,0 +1,16 @@
+var photoSrc = document.getElementById('UserImg');
+var Status = document.getElementById("Status");
+var title = document.getElementById('Title');
+var userName = document.getElementById('Name');
+var last_login = document.getElementById('lld');
+var current_date = document.getElementById('td');
+var code = document.getElementById('cus');
+var Msg = document.getElementById("Msg");
+
+Status.innerHTML.style = container.Status;
+title.innerHTML = container.Title;
+userName.innerHTML = container.userName;
+last_login.innerHTML = container.last_login;
+current_date.innerHTML = container.current_date;
+code.innerHTML = container.code;
+photoSrc.innerHTML = container.src;
---
+++ js/indexer.js
@@ -0,0 +1,9 @@
+var container = {
+  userName : "Test Member",
+  Title : "Member",
+  Status : "color: green;",
+  last_login : "2018/06/13 [20:46:21]",
+  current_date : "02/02/05",
+  code :"cus_D0nTkXIS0gUJ9Z",
+  src : "Jose.jpg"
+};
